74a97322fd3e03bab8f7c7369a7c543d69003566
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-19 12:25:51 -0800

    Update files

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(function(){
-
-  $('#styleMe input[type="text"]').blur(function(){
+  $('.styleMe input[type="text"]').blur(function(){
     if($(this).val().length > 0){
       $(this).addClass('white');
     } else {
@@ -11,44 +10,53 @@ $(function(){
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedStreet = $("input#new-street").val();
-    var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
-    var inputtedZip = $("input#new-zip").val();
-    var newAddress = { street: inputtedStreet,
-                       city: inputtedCity,
-                       state: inputtedState,
-                       zip: inputtedZip,
-                       fullAddress: function() {
-                        return this.street + ", " + this.city + ", " + this.state + ", " + this.zip;
-                       }
-                     };
+    var inputtedFirstName = $("input.new-first-name").val();
+    var inputtedLastName = $("input.new-last-name").val();
     var newContact = {firstName: inputtedFirstName,
                       lastName: inputtedLastName,
-                      address: newAddress.fullAddress()
+                      addresses: []
                       };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedZip = $(this).find("input.new-zip").val();
+
+      var newAddress = { street: inputtedStreet,
+                          city: inputtedCity,
+                          state: inputtedState,
+                          zip: inputtedZip
+                        };
+      newContact.addresses.push(newAddress);
+    });
+    // alert(newContact.firstName);
+
 
     $("ul#contact-list").append("<li><i class='fa-li fa fa-home'></i><span class='contact link'>" + newContact.firstName + " " + newContact.lastName + "</span><p> Add to Favorites?  <input type='checkbox' class='favorite'></p></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-street").val("");
-    $("input#new-city").val("");
-    $("input#new-state").val("");
-    $("input#new-zip").val("");
+    $("input.new-first-name").val("");
+    $("input.new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-zip").val("");
 
     var addLink = function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+      // $(".address").text(newContact.address);
+
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + ", " + address.zip + "</li>");
+      });
     };
 
-    $(".contact").last().click(addLink);
 
+    $(".contact").last().click(addLink);
     var sortL = function($list) {
       var alphaList = $list;
       var listContacts = alphaList.children('li').get();
@@ -76,7 +84,45 @@ $(function(){
     });
     sortL($("#contact-list"));
     sortL($("#favorite-list"));
-
-
   });
+
+    $("#add-address").click(function() {
+      $("#new-addresses").append('<div class="new-address">' +
+          '<div class ="row">' +
+            '<div class="col-md-3">' +
+              '<div class="form-group styleMe">' +
+                '<label  class="input" >' +
+                  '<input type="text" class="new-street" required>' +
+                  '<span><span>Street</span></span>' +
+                '</label>' +
+              '</div>' +
+            '</div>' +
+            '<div class="col-md-3">' +
+              '<div class="form-group styleMe">' +
+                '<label  class="input" >' +
+                  '<input type="text" class="new-city" required>' +
+                  '<span><span>City</span></span>' +
+                '</label>' +
+              '</div>' +
+            '</div>' +
+            '<div class="col-md-3">' +
+              '<div class="form-group styleMe">' +
+                '<label  class="input" >' +
+                  '<input type="text" class="new-state" required>' +
+                  '<span><span>State</span></span>' +
+                '</label>' +
+              '</div>' +
+            '</div>' +
+            '<div class="col-md-3">' +
+              '<div class="form-group styleMe">' +
+                '<label  class="input" >' +
+                  '<input type="text" class="new-zip" required>' +
+                  '<span><span>Zip</span></span>' +
+                '</label>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>');
+    });
+
 });
